5a517d02df105ea51f64f29ba8a841cd28e124f1
naruto1031 <naruto0614@au.com>
2022-07-22 00:05:39 +0900

    要素のリサイズ及び要素のpositionの取得を可能に

--- prototype_edit/js/edit.js
+++ prototype_edit/js/edit.js
@@ -1,8 +1,9 @@
-function init() {
-	// jquery uiのライブラリ
-	$("#targetDiv").resizable({});
-
-	$("#targetDiv").draggable({});
-}
-
-window.onload = init
+$(".move3").draggable({ containment: "#w3"});
+$(".move3").resizable({});
+$("#posi").bind("mousemove", function(){
+	var left = $("#posi").css("left")
+    var top = $('#posi').css("top");
+	debugger;
+    $('#left').val(left);
+    $('#top').val(top);
+});
