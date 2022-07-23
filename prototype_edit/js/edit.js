219f74b7288e3727cf001f63eb4ea8225540a5bb
naruto1031 <naruto0614@au.com>
2022-07-23 23:15:44 +0900

    ダブルクリックで文字編集を可能にし、ドラッグ移動を無効化を可能に。

--- prototype_edit/js/edit.js
+++ prototype_edit/js/edit.js
@@ -1,9 +1,18 @@
-$(".move3").draggable({ containment: "#w3"});
+$(".move3").draggable({containment: "#w3"});
 $(".move3").resizable({});
-$("#posi").bind("mousemove", function(){
-	var left = $("#posi").css("left")
-    var top = $('#posi').css("top");
-	// debugger;
-    $('#left').val(left);
-    $('#top').val(top);
+
+// $("#posi").bind("mousemove", function(){
+// 	var left = $("#posi").css("left")
+//     var top = $('#posi').css("top");
+// 	debugger;
+//     $('#left').val(left);
+//     $('#top').val(top);
+// });
+
+$(".move4").dblclick(()=>{
+    debugger;
+    $(".move3").draggable("destroy" );
+});
+$(".wrap").on("click",()=>{
+    $(".move3").draggable({containment: "#w3"});
 });
