84f2c1131b9bb9cea6cc2cda391828906be58f65
naruto1031 <naruto0614@au.com>
2022-07-24 10:34:24 +0900

    不具合改善のため、リサイズを無効化中

--- prototype_edit/js/edit.js
+++ prototype_edit/js/edit.js
@@ -1,6 +1,24 @@
-$(".move3").draggable({containment: "#w3"});
-$(".move3").resizable({});
+// $(".move3").draggable({containment: "#w3"});
 
+
+// $(".move4").dblclick((e)=>{
+//     debugger;
+//     e.stopPropagation();
+//     $(".move3").draggable("destroy");
+//     $(".move3").addClass("boder");
+// });
+$(".move3").on("click",(e)=>{
+    debugger;
+    e.stopPropagation();
+    $(".move3").draggable("destroy");
+    $(".move3").addClass("boder");
+});
+$(".wrap").on("click",()=>{
+    $(".move3").draggable({containment: "#w3"});
+    $(".move3").removeClass("boder");
+});
+
+// 現状不要処理(いったん残しとく)
 // $("#posi").bind("mousemove", function(){
 // 	var left = $("#posi").css("left")
 //     var top = $('#posi').css("top");
@@ -8,11 +26,3 @@ $(".move3").resizable({});
 //     $('#left').val(left);
 //     $('#top').val(top);
 // });
-
-$(".move4").dblclick(()=>{
-    debugger;
-    $(".move3").draggable("destroy" );
-});
-$(".wrap").on("click",()=>{
-    $(".move3").draggable({containment: "#w3"});
-});
