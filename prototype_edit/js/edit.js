8f9cb130ce112d9c3be0833cc9ae3c740655cf60
naruto1031 <naruto0614@au.com>
2022-07-29 21:04:25 +0900

    補完機能用typescriptファイル格納 コード整理

--- prototype_edit/js/edit.js
+++ prototype_edit/js/edit.js
@@ -1,5 +1,21 @@
 // $(".move3").draggable({containment: "#w3"});
+$(".move3").on("click",(e)=>{
+    debugger;
+    e.stopPropagation();
+    $(".move3").draggable("destroy");
+    $(".move3").addClass("boder");
+});
 
+$(".wrap").on("click",()=>{
+    $(".move3").draggable({containment: "#w3"});
+    $(".move3").removeClass("boder");
+});
+
+$(".move3").hover(()=>{
+    $(".move3").addClass("boder");
+},()=>{
+    $(".move3").removeClass("boder");
+});
 
 // $(".move4").dblclick((e)=>{
 //     debugger;
@@ -7,18 +23,7 @@
 //     $(".move3").draggable("destroy");
 //     $(".move3").addClass("boder");
 // });
-$(".move3").on("click",(e)=>{
-    debugger;
-    e.stopPropagation();
-    $(".move3").draggable("destroy");
-    $(".move3").addClass("boder");
-});
-$(".wrap").on("click",()=>{
-    $(".move3").draggable({containment: "#w3"});
-    $(".move3").removeClass("boder");
-});
-
-// 現状不要処理(いったん残しとく)
+//// 現状不要処理(いったん残しとく)
 // $("#posi").bind("mousemove", function(){
 // 	var left = $("#posi").css("left")
 //     var top = $('#posi').css("top");
