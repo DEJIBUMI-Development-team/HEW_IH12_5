946588d1b9795047f67a6a4a37f91d0559331419
yamyam10 <tapioka2real@gmail.com>
2022-10-14 10:54:13 +0900

    mypage.php、mypage.css変更

--- assets/php/mypage.php
+++ assets/php/mypage.php
@@ -1,21 +1,3 @@
-<?php
-session_start();
-require_once '../classes/UserLogic.php';
-require_once '../functions.php';
-
-//　ログインしているか判定し、していなかったら新規登録画面へ返す
-$result = UserLogic::checkLogin();
-
-if (!$result) {
-  $_SESSION['login_err'] = 'ユーザを登録してログインしてください！';
-  header('Location: signup_form.php');
-  return;
-}
-
-$login_user = $_SESSION['login_user'];
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -27,12 +9,10 @@ $login_user = $_SESSION['login_user'];
 </head>
 <body>
 <h2>マイページ</h2>
-<p>ログインユーザ：<?php echo h($login_user['name']) ?></p>
-<p>メールアドレス：<?php echo h($login_user['email']) ?></p>
+<p>ログインユーザ：<?php //echo h($login_user['name']) ?></p>
+<p>メールアドレス：<?php //echo h($login_user['email']) ?></p>
 <form action="logout.php" method="POST">
 <input type="submit" name="logout" value="ログアウト">
 </form>
-<!-- ①ログアウト画面の作成 -->
-<!-- ②ログアウトメソッドの作成 -->
 </body>
 </html>
