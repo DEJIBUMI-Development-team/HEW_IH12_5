6b913e424bc24ec9d3d63a38cf81387b0e1d487e
naruto1031 <naruto0614@au.com>
2022-10-27 11:37:39 +0900

    develop edit-txt-svg (evacation commit)

--- assets/php/edit.php
+++ assets/php/edit.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/edit.css">
     <title>Document</title>
 </head>
-    <body>
-        
-    </body>
+<body>
+    <div class="contentbox">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+            <foreignObject width="100%" height="100%" x="0" y="0">
+                <div xmlns="http://www.w3.org/1999/xhtml" contenteditable="true" class="text">
+                    <text>コンテンツ</text>
+                </div>
+            </foreignObject>
+        </svg> 
+    </div>
+<script src="../js/edit.js"></script>
+</body>
 </html>
