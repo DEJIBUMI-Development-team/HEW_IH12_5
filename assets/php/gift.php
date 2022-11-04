daf0d27ecc55e2256ac52ccd694749f47ab8fd93
duwns5769 <duwns5769@naver.com>
2022-11-04 09:48:26 +0900

    now conflict commit

---
+++ assets/php/gift.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>でじぶみ</title>
+  <link rel="stylesheet" href="../css/gift.css">
+</head>
+<body>  
+
+<div class="background">
+ <div class="water"></div>
+</div>
+
+<header class="navigator">
+  <div class="navigator_content">
+    <div class="navigator_left">
+      <a href="index.html"><img src="../img/logo.png" alt="logo"></a>
+      <a href="">手紙一覧</a>
+      <a href="">ギフト</a>
+      <a href="">マイページ</a>
+    </div>
+
+    <div class="navigator_right">
+      <a href="">会員登録</a>
+      <a href="">ログイン</a>
+    </div>
+  </div>
+</header>
+
+<!-- info-box -->
+<div class="info" id="info">
+  <article class="content">
+    <div class="content_img">
+      <img src="image/cup/1.jpg" alt="">
+    </div>
+
+    <section class="content_text">
+      <h2>cup</h2>
+        <p>説明らんです！説明らんです！説明らんです！説明らんです！説明らんです！説明らんです！</p>
+          <form><button class="cart_btn">詳細はこちら</button></form> 
+    </section>
+  </article>
+</div>
+
+<!-- 選択-box -->
+<div class="select_box">
+  <select class="item_select">
+    <option value="">おすすめ順</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+  </select>
+</div>
+
+<h3 class="item_list">全201商品</h3>
+
+<article class="item" id="item-section">
+  <div class="box00"> 
+  <div class="item_box">
+
+  <section class="ite">
+    <a href=""><img src="image/cup/1.jpg"></a>
+      <h3>だらま</h3>
+      <p>¥500</p>
+  </section>
+  
+  <section class="ite">
+    <a href=""><img src="image/cup/2.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+  
+  <section class="ite">
+    <a href=""><img src="image/cup/3.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+  
+  <section class="ite">
+    <a href=""><img src="image/cup/4.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+  
+  <section class="ite">
+    <a href=""><img src="image/cup/5.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+  
+  <section class="ite">
+    <a href=""><img src="image/cup/6.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+
+  <section class="ite">
+    <a href=""><img src="image/cup/7.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+
+  <section class="ite">
+    <a href=""><img src="image/cup/8.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+
+  <section class="ite">
+    <a href=""><img src="image/cup/9.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+
+  <section class="ite">
+    <a href=""><img src="image/cup/10.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+
+  <section class="ite">
+    <a href=""><img src="image/cup/11.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+
+  </section><section class="ite">
+    <a href=""><img src="image/cup/12.jpg"></a>
+      <h3>タイトル</h3>
+      <p>¥値段</p>
+  </section>
+  </div>
+  </div>
+</article>
+
+</body>
+</html>
