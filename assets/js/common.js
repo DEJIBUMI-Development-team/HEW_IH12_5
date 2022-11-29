c67e26ac08ae81ad2930b4cc18948cefa92746d2
yellowlemon22 <yellowlemon1106@gaml.com>
2022-11-29 16:43:27 +0900

    仮完成

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,12 +6,12 @@
 
 
 
-$(window).load(function(){
-  scrolltop();
-  pulldown();
-  $('.target').delay(500).scrollClass();
-  //cal();
-});
+// $(window).load(function(){
+//   scrolltop();
+//   pulldown();
+//   $('.target').delay(500).scrollClass();
+//   //cal();
+// });
 
 
 /*********************************************************************************************/
@@ -58,30 +58,30 @@ function scrolltop(){
 /*********************************************************************************************/
 // スロールに合わせてclass追加
 /*********************************************************************************************/
-(function($){
-  $.fn.scrollClass = function(config){
-      var defaults = {};
-      var config = $.extend(defaults, config);
-      var target = this;
+// (function($){
+//   $.fn.scrollClass = function(config){
+//       var defaults = {};
+//       var config = $.extend(defaults, config);
+//       var target = this;
 
-      function addAction(){
-          var length = target.length;
-          for(var i=0; i<length; i++){
-              if(target.eq(i).hasClass('action')) continue;
+//       function addAction(){
+//           var length = target.length;
+//           for(var i=0; i<length; i++){
+//               if(target.eq(i).hasClass('action')) continue;
               
-              var in_position = target.eq(i).offset().top + 100;
-              var window_bottom_position = $(window).scrollTop() + $(window).height();
-              if(in_position < window_bottom_position){
-                  target.eq(i).addClass('action');
-              }
-          }
-      }
-      addAction();
+//               var in_position = target.eq(i).offset().top + 100;
+//               var window_bottom_position = $(window).scrollTop() + $(window).height();
+//               if(in_position < window_bottom_position){
+//                   target.eq(i).addClass('action');
+//               }
+//           }
+//       }
+//       addAction();
 
-      $(window).on('scroll', $.throttle(250, function(){
-          addAction();
-      }));
-      return target;
-  };
-} )(jQuery);
+//       $(window).on('scroll', $.throttle(250, function(){
+//           addAction();
+//       }));
+//       return target;
+//   };
+// } )(jQuery);
 /*********************************************************************************************/
