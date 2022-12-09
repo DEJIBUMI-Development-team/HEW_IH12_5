48760a05ab864e48692c33e8477c7d7bc11ad759
duwns5769 <duwns5769@naver.com>
2022-12-09 12:29:25 +0900

    create gift page

--- assets/php/gift.php
+++ assets/php/gift.php
@@ -3,10 +3,9 @@
 <head>
   <meta charset="utf-8">
   <title>でじぶみ</title>
-  <link rel="stylesheet" href="../css/gift.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@400;500;700&family=Shippori+Mincho:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/gift1.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 </head>
 <body>  
 
@@ -30,118 +29,443 @@
   </div>
 </header>
 
-<!-- info-box -->
-<div class="info" id="info">
-  <article class="content">
-    <div class="content_img">
-      <img src="image/cup/1.jpg" alt="">
+<!-- Main image -->
+<div class="image-box">
+   <img src="./image/main.png">
+</div>
+
+<section class="gift_list">
+  <div class="gift_list_content">
+    <a href=""><img src="../img/list.jpg" alt=""><p>NEW GIFTS</p></a>
+    <a href=""><img src="../img/list1.jpg" alt=""><p>誕生日 プレゼント</p></a>
+    <a href=""><img src="../img/list2.jpg" alt=""><p>配達 プレゼント</p></a>
+    <a href=""><img src="../img/list3.jpg" alt=""><p>結婚 プレゼント</p></a>
+    <a href=""><img src="../img/list5.jpg" alt=""><p>軽い プレゼント</p></a>
+    <a href=""><img src="../img/list4.jpg" alt=""><p>健康・サプリメント</p></a>
+  </div>
+</section>
+
+<section class="category_list">
+  <div class="category_list_title">
+   <h1>カテゴリー</h1> 
+  </div>
+
+  <div class="category_list_content">
+    <div class="multiple-items">
+      <div><a href=""><img src="../img/ca1.jpg" alt="ca1"><p>コーヒー & 飲料</p></a></div>
+      <div><a href=""><img src="../img/ca2.jpg" alt="ca2"><p>ケーキ & スイーツ</p></a></div>
+      <div><a href=""><img src="../img/ca3.png" alt="ca3"><p>花束 & 果物セット</p></a></div>
+      <div><a href=""><img src="../img/ca4.jpg" alt="ca4"><p>ギフトセット</p></a></div>
+      <div><a href=""><img src="../img/ca5.jpg" alt="ca5"><p>健康商品</p></a></div>
+      <div><a href=""><img src="../img/ca6.jpg" alt="ca6"><p>ギフト券</p></a></div>
+      <div><a href=""><img src="../img/ca7.jpg" alt="ca7"><p>マーケット</p></a></div> 
+      <div><a href=""><img src="../img/ca8.jpg" alt="ca8"><p>ハンバーガー&ピザ</p></a></div>
+      <div><a href=""><img src="../img/ca9.jpg" alt="ca9"><p>コンビニ</p></a></div>
+      <div><a href=""><img src="../img/ca10.jpg" alt="ca10"><p>エンターテイメント</p></a></div>
+      <div><a href=""><img src="../img/ca11.jpg" alt="ca11"><p>ビューティー</p></a></div>
     </div>
+  </div>  
+</section>
 
-    <section class="content_text">
-      <h2>cup</h2>
-        <p>説明らんです！説明らんです！説明らんです！説明らんです！説明らんです！説明らんです！</p>
-          <form><button class="cart_btn">詳細はこちら</button></form> 
-    </section>
-  </article>
-</div>
-
-<!-- 選択-box -->
-<div class="select_box">
-  <select class="item_select">
-    <option value="">おすすめ順</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-  </select>
-</div>
-
-<h3 class="item_list">全201商品</h3>
-
-<article class="item" id="item-section">
-  <div class="box00"> 
-  <div class="item_box">
-
-  <section class="ite">
-    <a href=""><img src="image/cup/1.jpg"></a>
-      <h3>だらま</h3>
-      <p>¥500</p>
-  </section>
-  
-  <section class="ite">
-    <a href=""><img src="image/cup/2.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-  
-  <section class="ite">
-    <a href=""><img src="image/cup/3.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-  
-  <section class="ite">
-    <a href=""><img src="image/cup/4.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-  
-  <section class="ite">
-    <a href=""><img src="image/cup/5.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-  
-  <section class="ite">
-    <a href=""><img src="image/cup/6.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-
-  <section class="ite">
-    <a href=""><img src="image/cup/7.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-
-  <section class="ite">
-    <a href=""><img src="image/cup/8.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-
-  <section class="ite">
-    <a href=""><img src="image/cup/9.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-
-  <section class="ite">
-    <a href=""><img src="image/cup/10.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
-
-  <section class="ite">
-    <a href=""><img src="image/cup/11.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-
-  </section><section class="ite">
-    <a href=""><img src="image/cup/12.jpg"></a>
-      <h3>タイトル</h3>
-      <p>¥値段</p>
-  </section>
+ 
+<section class="best_list">
+  <div class="best_list_title">
+   <h1>🎁 人気商品リスト 🎁</h1> 
   </div>
+<div class="best_list_content">
+  <div class="best-items">
+    <div><img src="../img/best1.jpg" alt="best1"></div>
+    <div><img src="../img/best2.jpg" alt="best2"></div>
+    <div><img src="../img/best3.jpg" alt="best3"></div>
+    <div><img src="../img/best4.jpg" alt="best4"></div>
+    <div><img src="../img/best5.jpg" alt="best5"></div>
+    <div><img src="../img/best6.jpg" alt="best6"></div>
+    <div><img src="../img/best7.jpg" alt="best7"></div> 
+    <div><img src="../img/best8.jpg" alt="best8"></div>
+    <div><img src="../img/best9.jpg" alt="best9"></div>
+    <div><img src="../img/best10.jpg" alt="best10"></div>
+    <div><img src="../img/best11.jpg" alt="best11"></div>
   </div>
-</article>
+</div>  
+</section>
+
+<section class="brand_list">
+  <h1>人気ブランド</h1>
+  <div class="wrapper">
+    <div class="brand_list_category">
+        <ul class="tabs">
+          <li class="tab-link current" data-tab="tab-1">ドリンク☕</li>
+          <li class="tab-link" data-tab="tab-2">食べ物 🍕🍗</li>
+          <li class="tab-link" data-tab="tab-3">ケーキ🍰</li>
+          <li class="tab-link" data-tab="tab-4">ショッピング🛍️</li>
+        </ul>
+     </div>
+
+        <div id="tab-1" class="tab-content current">
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/Starbucks.png">
+              <p>スターバックス</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/star1.jpg"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/tullys.jpg">
+              <p>タリーズコーヒー</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/ty.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/doutor.jpeg">
+              <p>ドトール</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/dou1.jpg"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/komeda.jpg">
+              <p>コメダ珈琲</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/kome.jpg"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/saruta.png  ">
+              <p>猿田彦コーヒー</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/saruta1.webp"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/san.jpg">
+              <p>サンマルクカフェ</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/san1.jpg"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/kohikan.png">
+              <p>珈琲館</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/kohikan1.webp"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/pronto.jpg">
+              <p>プロント</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/pronto1.png"></a>
+            </div>
+          </section>
+         </div>
+
+        <div id="tab-2" class="tab-content">
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/pizza hut.png">
+              <p>ピザハット</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/pizza hut1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/domino.png">
+              <p>ドミノピザ</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/domino1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/KFC.png">
+              <p>ケンタッキー</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/KFC1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/yosinoya.png">
+              <p>吉野家</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/yosinoya1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/susiro.jfif">
+              <p>スシロー</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/susiro1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/mac.png">
+              <p>マクドナルド</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/mac1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/yayoi.png">
+              <p>やよい軒</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/yayoi1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/ootoya.jfif">
+              <p>大戸屋</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/ootoya1.jfif"></a>
+            </div>
+          </section>
+        </div>
+
+        <div id="tab-3" class="tab-content">
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/31.jfif">
+              <p>バスキン・ロビンス</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/31-1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/misudo.png">
+              <p>ミスタードーナツ</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/misudo1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/hujiya.png">
+              <p>不二家</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/hujiya1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/ginza.png">
+              <p>銀座コージーコーナー</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/ginza1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/godiva.png">
+              <p>ゴディバ</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/godiva1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/chate.png">
+              <p>シャトレーゼ</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/chate1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/hagen.png">
+              <p>ハーゲンダッツ</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/hagen1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/mochi.jfif">
+              <p>もち吉</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/mochi1.jfif"></a>
+            </div>
+          </section>
+        </div>
+
+        <div id="tab-4" class="tab-content">
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/zozo.png">
+              <p>zozotown</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/zozo1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/lush.png">
+              <p>Lush</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/lush1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/amazon.png">
+              <p>amazon</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/amazon1.png"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/apple.png">
+              <p>apple store</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/apple1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/isetan.png">
+              <p>伊勢丹</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/isetan1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/JTB.png">
+              <p>JTB</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/JTB1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/muji.png">
+              <p>無印良品</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/muji1.jfif"></a>
+            </div>
+          </section>
+
+          <section class="brand_item">
+            <a href="">
+            <div class="item_name">
+              <img src="../img/aoki.png">
+              <p>AOKI</p>
+            </div>
+            <div class="item_list">
+              <img src="../img/aoki1.jfif"></a>
+            </div>
+          </section>
+        </div>
+</section>
 
 <div class="box00">
   <nav class="haitatsu">
     <ul>
-    <li><a href=""><img src="../img/haitatsu.png"></a></li>
+    <li><img src="../img/haitatsu.png"></li>
     </ul>
   </nav>
 </div>
@@ -151,9 +475,9 @@
   <div class="top_element">
     <div class="top_inner">
       <ul class="footer_menu">
-        <li><a href="">利用規約</a></li>
+        <li><a href="">利用約款</a></li>
         <li><a href=""><span>個人利用処理方針</span></a></li>
-        <li><a href="">お客様センター</a></li>
+        <li><a href="">客様センター</a></li>
         <li><a href="">意見提出</a></li>
       </ul>
 
@@ -175,8 +499,28 @@
     </div>
   </div>
 </footer>
-</body>
-</html>
+<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.6.1.js"
+  integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script>
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+});
+</script>
+
+<script>
+  $('.best-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+</script>
+<script src="../js/tab.js"></script>
 
 </body>
 </html>
