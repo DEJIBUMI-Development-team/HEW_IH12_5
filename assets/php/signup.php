bad5b87020bb21be5b54dc25954fa2c490a3ea44
yellowlemon22 <yellowlemon1106@gaml.com>
2022-12-13 01:03:31 +0900

    レイアウト修正

--- assets/php/signup.php
+++ assets/php/signup.php
@@ -41,39 +41,48 @@ if (!empty($_POST)) {
     <link rel="stylesheet" href="../css/signup.css">
 </head>
 <body>
-    <div class="content">
-        <form action="" method="POST">
-            <h1>アカウント作成</h1>
-            <p>でじぶみをご利用するために、次のフォームに必要事項をご記入ください。</p>
-            <br>
- 
-            <div class="control">
-                <label for="name">ユーザー名</label>
-                <input id="name" type="text" name="name">
-            </div>
- 
-            <div class="control">
-                <label for="email">メールアドレス<span class="required">必須</span></label>
-                <input id="email" type="email" name="email">
-                <?php if (!empty($error["email"]) && $error['email'] === 'blank'): ?>
-                    <p class="error">＊メールアドレスを入力してください</p>
-                <?php elseif (!empty($error["email"]) && $error['email'] === 'duplicate'): ?>
-                    <p class="error">＊このメールアドレスはすでに登録済みです</p>
-                <?php endif ?>
-            </div>
- 
-            <div class="control">
-                <label for="password">パスワード<span class="required">必須</span></label>
-                <input id="password" type="password" name="password">
-                <?php if (!empty($error["password"]) && $error['password'] === 'blank'): ?>
-                    <p class="error">＊パスワードを入力してください</p>
-                <?php endif ?>
-            </div>
- 
-            <div class="control">
-                <button type="submit" class="btn">確認する</button>
-            </div>
-        </form>
-    </div>
+    <main>
+        <div id="backgrounds">
+                <div class="water1"></div>
+        </div>
+        <div id="first_view">
+            <img class="icon" src="../img/logo.png" alt="">
+        </div>
+        <div class="content">
+            <form action="" method="POST">
+                <h1>アカウント作成</h1>
+                <p>でじぶみをご利用するために、次のフォームに必要事項をご記入ください。</p>
+                <br>
+    
+                <div class="control">
+                    <label for="name">ユーザー名</label>
+                    <input id="name" type="text" name="name">
+                </div>
+    
+                <div class="control">
+                    <label for="email">メールアドレス<span class="required">必須</span></label>
+                    <input id="email" type="email" name="email">
+                    <?php if (!empty($error["email"]) && $error['email'] === 'blank'): ?>
+                        <p class="error">＊メールアドレスを入力してください</p>
+                    <?php elseif (!empty($error["email"]) && $error['email'] === 'duplicate'): ?>
+                        <p class="error">＊このメールアドレスはすでに登録済みです</p>
+                    <?php endif ?>
+                </div>
+    
+                <div class="control">
+                    <label for="password">パスワード<span class="required">必須</span></label>
+                    <input id="password" type="password" name="password">
+                    <?php if (!empty($error["password"]) && $error['password'] === 'blank'): ?>
+                        <p class="error">＊パスワードを入力してください</p>
+                    <?php endif ?>
+                </div>
+    
+                <div class="control">
+                    <button type="submit" class="btn">確認する</button>
+                    <button type="submit" class="btn">ログインする</button>
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
