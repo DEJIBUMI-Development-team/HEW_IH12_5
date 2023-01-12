e03b867f5b6f9843b380637b711eda4360b1a3fb
naruto1031 <naruto0614@au.com>
2023-01-12 11:22:37 +0900

    develop new creation func

--- assets/php/mypage.php
+++ assets/php/mypage.php
@@ -7,12 +7,25 @@ if (!empty($_SESSION["user_id"])) {
 } else {
 	$user_id = 1;
 }
-$data =  db("SELECT edit_id, title FROM t_user_edit where F_user_id = {$user_id}");
+
+if (isset($_POST["delete"])) {
+	$delete_edit_id = $_POST["delete_id"];
+	db("DELETE FROM t_user_edit WHERE edit_id = {$delete_edit_id}");
+	$_POST = array();
+}
+
+if (isset($_POST["new"])) {
+	unset($_SESSION["edit_id"]);
+	header("Location:./edit.php");
+}
+
+
+$data = db("SELECT edit_id, title FROM t_user_edit where F_user_id = {$user_id}");
 $data = json_encode($data);
 
 if (isset($_POST["logout"])) {
 	$_SESSION = array();
-	header("Location:../../index.php");		
+	header("Location:../../index.php");
 	session_destroy();
 }
 ?>
@@ -32,7 +45,9 @@ if (isset($_POST["logout"])) {
 	<h2>でじぶみページ</h2>
 	<div class="user_icon">
 		<img src="../img/user_icon.svg" alt="マイページ">
-		<p><?php echo $_SESSION["user_name"]; ?></p>
+		<p>
+			<?php echo $_SESSION["user_name"]; ?>
+		</p>
 	</div>
 	<div class="container">
 		<div class="item n01">
@@ -48,7 +63,9 @@ if (isset($_POST["logout"])) {
 		</div>
 	</div>
 	<div class="view-history" id="history">
-		<div class="new_edit">でじぶみを新規作成</div>
+		<form class="new_edit" id="new_creation" method="POST" onsubmit="newSubmit()">
+			<input type="submit" name="new" value="でじぶみを新規作成">
+		</form>
 	</div>
 	<form action="" method="POST">
 		<input type="submit" name="logout" value="ログアウト">
@@ -60,7 +77,7 @@ if (isset($_POST["logout"])) {
 			return data;
 		}
 
-		window.onload = async function() {
+		window.onload = async function () {
 			try {
 				// debugger;
 				var insert_pr = document.getElementById("history");
@@ -76,16 +93,13 @@ if (isset($_POST["logout"])) {
 					insert_pr.insertAdjacentHTML("beforeend", create_history_dom("", ""));
 				}
 
-				const redraw_element = document.querySelectorAll(".history");
-				redraw_element.forEach((element) => {
+				const redraw_element = document.querySelectorAll(".history-content");
+				redraw_element.forEach(element => {
 					element.addEventListener("click", (e) => {
 						var id = get_id(e, "edit_id");
 						location.href = "./edit.php?edit_id=" + encodeURIComponent(id);
 					});
 				});
-
-
-
 			} catch (error) {
 				console.log(error);
 			}
@@ -95,13 +109,20 @@ if (isset($_POST["logout"])) {
 		function create_history_dom(id, title) {
 			if (!id && !title) {
 				return `
-            		<div class="history-content">編集した手紙のデータはありません</div>
-        		`;
+					<div class="history-content">編集した手紙のデータはありません</div>
+				`;
 			}
 
 			return `
-				<div class="history-content history" data-edit_id="${id}">
-					<div class="history-id" data-edit_id="${id}">編集画面ID : ${id}</div><div class="history-tile" data-edit_id="${id}">タイトル : ${title}</div>
+				<div class="history-content" data-edit_id="${id}">
+					<div class="history" data-edit_id="${id}">
+						<div class="history-id" data-edit_id="${id}">編集画面ID : ${id}</div>
+						<div class="history-title" data-edit_id="${id}">タイトル : ${title}</div>
+					</div>
+					<form method="POST" class="delete" onsubmit="return deleteSubmit()">
+						<input type="hidden" name="delete_id" value="${id}">
+						<input type="submit" name="delete" value="削除する">
+					</form>
 				</div>
 			`;
 		}
@@ -109,6 +130,20 @@ if (isset($_POST["logout"])) {
 		function get_id(event, specified_key) {
 			clickedDom = event.composedPath();
 			return clickedDom[0].dataset[specified_key];
+		}
+		function deleteSubmit() {
+			if (window.confirm('このでじぶみデータを削除しますが、よろしいですか？')) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		function newSubmit() {
+			if (window.confirm('でじぶみを新規作成しますか？')) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 	</script>
 </body>
