333f34fa03a51746c82409b148cc6641a0fd36da
duwns5769 <duwns5769@naver.com>
2023-01-13 10:06:34 +0900

    syohin

---
+++ assets/php/shohin.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>でじぶみ</title>
+  <link rel="stylesheet" href="./css/shohin.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+</head>
+<body>  
+
+<div class="background">
+ <div class="water"></div>
+</div>
+
+<header class="navigator">
+  <div class="navigator_content">
+    <div class="navigator_left">
+      <a href="index.html"><img src="./image/logo.png" alt="logo"></a>
+      <a href="">手紙一覧</a>
+      <a href="gift.html">ギフト</a>
+      <a href="">マイページ</a>
+    </div>
+
+    <div class="navigator_right">
+      <a href="">会員登録</a>
+      <a href="">ログイン</a>
+    </div>
+  </div>
+</header>
+
+<!-- map-box -->
+<div class="map_wrap">
+  <div class="map_inner">
+    <div class="map_channer">
+      <ul class="map_list">
+        <li><a href="index.html">Home</a></li>
+        <li class="line"><a href="customer.html">顧客センター</a></li>
+        <li class="line"><a href="info.html">最新情報</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<!-- itme-box -->
+<div class="item_wrap">
+  <div class="item_meta">
+    <div class="item_meta_image">
+      <img src="./image/starbucks/stame.jpg">
+    </div>
+    <div class="item_meta_font">
+      <h2>スターバックス</h2>
+    </div>
+  </div>
+</div>
+
+<!-- footer-box -->
+<footer class="footer_element">
+  <div class="top_element">
+    <div class="top_inner">
+      <ul class="footer_menu">
+        <li><a href="">利用規約</a></li>
+        <li><a href=""><span>個人利用処理方針</span></a></li>
+        <li><a href="">お客様センター</a></li>
+        <li><a href="">意見提出</a></li>
+      </ul>
+
+      <ul class="sns">
+        <li><a href=""><i class="fab fa-twitter"></i></a></li>
+        <li><a href=""><i class="fab fa-facebook"></i></a></li>
+        <li><a href=""><i class="fab fa-instagram"></i></a></li>
+        <li><a href=""><i class="fab fa-youtube"></i></a></li>
+      </ul>  
+    </div>
+  </div>
+
+  <div class="body_element">
+    <div class="body_inner">
+      <p class="footer_logo"><a href=""><img src="./image/logo.png"></a></p>
+      <p class="footer_info">dejibumi  コールセンター 070-1234-5678 (09:00 ~ 18:00 平日)
+      <br>
+      Copyright © dejibumi. All Rights reserved.</p>
+    </div>
+  </div>
+</footer>
