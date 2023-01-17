499ad8c016e93db096c5e2e9dc2ae01a191b39d6
naruto1031 <naruto0614@au.com>
2023-01-18 00:01:09 +0900

    fix design create_result page

--- assets/php/create_result.php
+++ assets/php/create_result.php
@@ -6,78 +6,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        /* CSS */
-        body,
-        html {
-            margin: 0;
-            padding: 0;
-        }
-
-        body {
-            /* background-image: url(../img/water_blue1.jpg); */
-            background-color: rgb(248, 248, 215);
-            background-size: cover;
-            background-repeat: no-repeat;
-            width: 100%;
-        }
-
-        .text-erea {
-            background-color: white;
-            padding: 2px;
-        }
-
-        .text-erea p {
-            font-size: 18px;
-            background-color: white;
-            padding-left: 4px;
-            white-space: nowrap;
-            /* 横幅のMAXに達しても改行しない */
-            overflow: hidden;
-            /* ハミ出した部分を隠す */
-            text-overflow: ellipsis;
-            /* 「…」と省略 */
-            -webkit-text-overflow: ellipsis;
-            /* Safari */
-            -o-text-overflow: ellipsis;
-            /* Opera */
-        }
-
-        button {
-            font-size: 16px;
-            background-color: green;
-            color: white;
-            border: none;
-            padding: 8px 16px;
-            cursor: pointer;
-        }
-
-        .url-clip-boad {
-            width: 600px;
-            margin: 20% auto;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/create_result.css">
 </head>
 
 <body>
-    <div class="url-clip-boad">
-        <div class="text-erea">
-            <p id="target-text"><?php echo "http://localhost/HEW_IH12_5/assets/php/letter.php?" . http_build_query($_GET); ?></p>
+    <div class="wrapper">
+        <div class="clip-title">
+            <div class="title-text">
+                <h2>でじぶみURLが発行されました</h2>
+                <h2>大切な相手に気持ちを伝えよう！！！</h2>
+            </div>
         </div>
-        <button id="copy_text">Copy URL</button>
-        <div class="copy-result"></div>
+        <div class="url-clip-boad">
+            <div class="text-erea">
+                <p id="target-text"><?php echo "http://localhost/HEW_IH12_5/assets/php/letter.php?" . http_build_query($_GET); ?></p>
+            </div>
+            <button id="copy_text">Copy URL</button>
+            <div class="copy-result"></div>
+        </div>
     </div>
+
     <script>
         const copy_button = document.getElementById("copy_text");
         copy_button.addEventListener("click", copyText);
         const focus_elem = document.querySelectorAll(".text-erea");
 
-        focus_elem.forEach((elem)=>{
-            elem.addEventListener("focus", ()=>{
+        focus_elem.forEach((elem) => {
+            elem.addEventListener("focus", () => {
                 console.log("focus");
-            })
-        })
-        
+            });
+        });
+
         function copyText() {
             // Select the text
             var elem = document.querySelector(".copy-result");
@@ -95,7 +54,6 @@
                 elem.textContent = msg;
                 console.log('Oops, unable to copy');
             }
-
             // Remove the selected text
             window.getSelection().removeAllRanges();
         }
