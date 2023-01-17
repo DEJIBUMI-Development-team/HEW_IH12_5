8187750ce498c9f61fea7d887e8d40220f71a2ce
yellowlemon22 <yellowlemon1106@gaml.com>
2023-01-17 16:49:26 +0900

    吹き出しに苦戦中

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,14 +8,42 @@ $(window).on('beforeunload', function(e){
   console.log('beforeunload');
 });
 
-// スクロールアニメーション
+// 右下固定説明動画固定ウィンドウ
 
+window.addEventListener("scroll", function () {
+  const elm = document.querySelector(".elm");
+  const scroll = window.pageYOffset;
+  if (scroll > 70) {
+    elm.style.opacity = "1";
+    // console.log(scroll);
+  } else {
+    elm.style.opacity = "0";
+    // console.log(scroll);
+  }
+});
 
-// 右下固定edit遷移ボタン
+// window.addEventListener("scroll", function () {
+//   const topBtn = document.getElementById("topbutton");
+//   const scroll = window.pageYOffset;
+//   if (scroll > 5) {      // ➃
+//     topBtn.style.opacity = 1;
+//   } else topBtn.style.opacity = 0; 
+// });
 
-
-// 左下固定説明動画固定ウィンドウ
-
+//モーダル表示
+$(".video-open").modaal({
+  // start_open:true, // ページロード時に表示するか
+  // overlay_close:true,//モーダル背景クリック時に閉じるか
+  type: 'video',
+  background: '#28BFE7', // 背景色
+  overlay_opacity:0.8, // 透過具合
+  // before_open:function(){// モーダルが開く前に行う動作
+  //   $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+  // },
+  // after_close:function(){// モーダルが閉じた後に行う動作
+  //   $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+  // }
+  });
 
 /* ------------------------------------------------
 first_content
