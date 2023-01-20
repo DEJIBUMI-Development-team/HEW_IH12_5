b4e48fe804e5350ff0977b1907d1f7e5c267507f
duwns5769 <duwns5769@naver.com>
2023-01-20 10:39:49 +0900

    ok

--- assets/php/shohin.php
+++ assets/php/shohin.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="../css/shohin.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+  
 </head>
 <body>  
 
@@ -35,8 +36,8 @@
     <div class="map_channer">
       <ul class="map_list">
         <li><a href="gift.html">ギフト</a></li>
-        <li class="line"><a href="starbucks.html">スターバックス</a></li>
-        <li class="line"><a href="shohin.html">カフェ アメリカーノ(Short~Venti)</a></li>
+        <li class="line"><a href="starbucks.php">スターバックス</a></li>
+        <li class="line"><a href="shohin.php">カフェ アメリカーノ(Short~Venti)</a></li>
       </ul>
     </div>
   </div>
@@ -81,18 +82,62 @@
                 <option value="4">4</option>
                 <option value="5">5</option>
               </select>
-              <button class="item_but">プレゼント</button>
+              <a href=""><button class="item_but">プレゼント</button></a>
               <hr>
             </div>
               <div class="item_brand-store">
-              <a href=""><img src="../img/Starbucks.png">
+              <a href="starbucks.html"><img src="../img/Starbucks.png">
               <p>スターバックス</p>
               </a>
-              </div>
+            </div>
         </div>
       </div>
     </div>
   </div>
+
+  <div class="item_info_under">
+    <ul class="tabs">
+      <li class="tab-link current" data-tab="tab-1">商品詳細</li>
+      <li class="tab-link" data-tab="tab-2">払い戻し&注意事項</li>
+    </ul>
+  
+    <div id="tab-1" class="tab-content current">
+      <p>• 有効期限は購入日から1年後に満了します。</p>
+      <br>
+      <p>有効期間内に使用できます。</p>
+
+      <p>- 写真画像は例であり、実際とは異なる場合があります。</p>
+      
+      <p>- 本商品は店舗在庫状況によって同一商品への交換が不可能な場合があります。</p>
+      
+      <P>- 同一商品の交換が不可能な場合、同一価格以上の他の商品に交換が可能で、差額は追加で支払わなければなりません。</P>
+      
+      <p>- ポイントの積み立てや提携カードの割引などは、交換先のポリシーに従います。</p>
+      
+      <p>- 該当クーポンとスターバックスカードの複合決済取引は、スターバックスカード固有の特典であるFree Extraおよび別積立金は適用対象ではない点、ご利用の際に参考にしてください。</p>
+      
+      <br>
+      <P>【使用場所】</P>
+      
+      <P>スターバックス全店舗</P>
+    </div>
+    <div id="tab-2" class="tab-content">
+      <span>•[有効期間延長政策]</span>
+      <br>
+      <P>1.有効期間の延長は未使用の引換券に限り有効期間満了日の1日前まで申請可能です。</P>
+
+      <p>2.有効期間は93日単位で延長可能ですが、これは最初の発行日から最大5年まで可能です。</p>
+
+      <p>3.有効期間の延長申請はinumberお客様センター(070-1234-5678)までお電話ください。 (祝日を除く平日午前9:00時~午後6:00時)</p>
+      <br>
+      <span>•[キャンセル及び払い戻しポリシー]</span>
+      <br>
+      <p>1.初回満了日前:使用された引換券はキャンセル及び払い戻しができず、未使用引換券に対する決済のキャンセル及び払い戻しは購入者がDEjibumiカスタマーセンター(support@dejibumi.com)に申請可能です。</p>
+
+      <p>2.最初の満了日後:受け取った方がDEjibumiお客様センター(070-1234-5678)までお電話いただくと申し込み可能で、購入金額の90%金額が払い戻しされます。</p>
+
+      <p>3. 有効期間が満了した引換券の払い戻し申請は、最初の発行日から5年以内まで可能です。</p>
+    </div>
   </div>
 </div>
 
@@ -125,3 +170,10 @@
     </div>
   </div>
 </footer>
+<script
+  src="https://code.jquery.com/jquery-3.6.1.js"
+  integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+  crossorigin="anonymous"></script>
+<script src="../js/tab.js"></script>
+</body>
+</html>
