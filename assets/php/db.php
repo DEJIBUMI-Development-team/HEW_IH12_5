97b0f764fba11c83dee068135b3e2f7cbc613d0a
naruto1031 <naruto0614@au.com>
2023-01-30 12:32:11 +0900

    fix mysql:host for db.php

--- assets/php/db.php
+++ assets/php/db.php
@@ -6,7 +6,7 @@ function db($sql): mixed{
     $db['username'] = "dejibumi";  // ユーザー名
     $db['password'] = "dejibumi-0";  // ユーザー名のパスワード
     $db['dbname'] = "dejibumi_db";  // データベース名
-    $dsn = 'mysql:host=dejibumi.sakura.ne.jp;dbname=dejibumi_db;charset=utf8';
+    $dsn = 'mysql:host=mysql57.dejibumi.sakura.ne.jp;dbname=dejibumi_db;charset=utf8';
     try {
         // Mysqlへの接続への接続を確立
         $pdo = new PDO($dsn, $db['username'], $db['password'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
