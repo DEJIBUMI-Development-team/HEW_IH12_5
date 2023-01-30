97cb50e359ddbdf2953361e49f923bcfd789685c
naruto1031 <naruto0614@au.com>
2023-01-30 12:41:16 +0900

    fix settelement.php

--- assets/php/settlement.php
+++ assets/php/settlement.php
@@ -1,23 +1,6 @@
 <?php
 session_start();
-function connectDB()
-{
-	$db['host'] = "mysql57.dejibumi.sakura.ne.jp";  // DBサーバのURL
-	$db['username'] = "dejibumi";  // ユーザー名
-	$db['password'] = "dejibumi-0";  // ユーザー名のパスワード
-	$db['dbname'] = "dejibumi_db";  // データベース名
-	$dsn = 'mysql:host=mysql57.dejibumi.sakura.ne.jp;dbname=dejibumi_db;charset=utf8';
-	try {
-		// Mysqlへの接続への接続を確立
-		$pdo = new PDO($dsn, $db['username'], $db['password'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-		return $pdo;
-	} catch (PDOException $e) {
-		exit($e->getMessage());	
-	}
-
-}
-
-$pdo = connectDB();
+include("./dbaccess.php");
 
 if (isset($_POST["settlement"])) {
 	$img_name = $_SESSION["img_name"];
