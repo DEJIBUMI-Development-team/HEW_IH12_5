f8f5e20b18f6e95dcfc836b5e86d571fa6a23968
yellowlemon22 <yellowlemon1106@gaml.com>
2023-02-16 10:13:29 +0900

    フォントの統一 TOPの画像差し替え

--- index.php
+++ index.php
@@ -25,10 +25,7 @@ if (isset($_POST['add'])) {
 	<title>でじぶみ</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/slick.css">
-	<link rel="stylesheet" href="assets/css/reset.css">
-	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="assets/css/animation.css">
-	<link rel="stylesheet" href="./assets/css/header.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Modaal/0.4.4/css/modaal.min.css">
 	<link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/9-6-2/css/9-6-2.css">
 	<link rel="stylesheet" href="./assets/css/header.css">
