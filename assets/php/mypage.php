29b64736b852381a5f684abdc94013e76a34d6b9
naruto1031 <naruto0614@au.com>
2023-02-20 11:12:13 +0900

    投票の開始終了を変更する処理を実装

--- assets/php/mypage.php
+++ assets/php/mypage.php
@@ -26,6 +26,32 @@ if (isset($_POST["new"])) {
 	header("Location:./edit.php");
 }
 
+if (isset($_POST["restart_survey"])) {
+	$open_survey_id = $_POST["open_id"];
+	$open_data = db("SELECT survey FROM t_user_letter where letter_id = {$open_survey_id}");
+	$open_data_json = json_decode($open_data[0]["survey"], true);
+	if ($open_data_json["ending"]){
+		$open_data_json["ending"] = false;
+		$encode_survey_data = json_encode($open_data_json, JSON_UNESCAPED_UNICODE);
+		db("UPDATE `t_user_letter` SET `survey` = '{$encode_survey_data}' WHERE `letter_id` = '{$open_survey_id}'");
+	}
+	header("Location:./mypage.php");
+	exit;
+}
+
+if (isset($_POST["finish_survey"])) {
+	$close_survey_id = $_POST["close_id"];
+	$close_data = db("SELECT survey FROM t_user_letter where letter_id = {$close_survey_id}");
+	$close_data_json = json_decode($close_data[0]["survey"], true);
+	if (!$close_data_json["ending"]){
+		$close_data_json["ending"] = true;
+		$encode_survey_data = json_encode($close_data_json, JSON_UNESCAPED_UNICODE);
+		db("UPDATE `t_user_letter` SET `survey` = '{$encode_survey_data}' WHERE `letter_id` = '{$close_survey_id}'");
+	}
+	header("Location:./mypage.php");
+	exit;
+}
+
 
 $data = db("SELECT edit_id, title FROM t_user_edit where F_user_id = {$user_id}");
 $data = json_encode($data);
@@ -39,6 +65,7 @@ if (isset($_POST["logout"])) {
 $survey_flg = false;
 $survey_data = db("SELECT letter_id, name, survey FROM t_user_letter where F_user_id = {$user_id}");
 $survey_result = array_column($survey_data, "survey");
+$survey_id = array_column($survey_data, "letter_id");
 
 if (isset($survey_result)){
 	$survey_flg = true;
@@ -48,12 +75,12 @@ if (isset($survey_result)){
 		if ($value == null) {
 			continue;
 		}
-		
-		$chert_contents[$key] = json_decode($value, true);
+		$chert_contents[$key] = [
+			json_decode($value, true),
+			$survey_id[$key]
+		];
 	}
-	// echo ("<pre>");
-	// var_dump($chert_contents);
-	// echo ("</pre>");
+
 };
 ?>
 
@@ -105,32 +132,56 @@ if (isset($survey_result)){
 			foreach ($chert_contents as $key => $chert) {
 				
 				echo <<<EOD
-					<h4>お返事名 : {$chert["title"]}</h4>
+					<h4>お返事名 : {$chert[0]["title"]}</h4>
+				EOD;
+				if (isset($chert[0]["ending"])) {
+					$result = "";
+					$chert_status = $chert[0]["ending"];
+					if (!$chert_status) {
+						$result =  "投票期間中";
+						echo <<<EOD
+						<form action="" method="POST" class="question-status">
+							<h4>{$result} : </h4>
+							<input type="submit" value="投票を終了する" name="finish_survey" class="finish">
+							<input type="hidden" name="close_id" value="{$chert[1]}">
+						</form>
+						EOD;
+					}else {
+						$result =  "投票終了";
+						echo <<<EOD
+						<form class="question-status" action="" method="POST">
+							<h4>{$result} : </h4>
+							<input type="submit" value="投票を再開する" name="restart_survey" class="finish">
+							<input type="hidden" name="open_id" value="{$chert[1]}">
+						</form>
+						EOD;
+					}
+				}
+				echo <<<EOD
 					<div class="question-content">
 						<div class="quertion">
 							<canvas id="myChart-{$key}"></canvas>
 						</div>
 						<div class="result-table">
 							<div class="table-content">
-								<div class="tbl-elem tbl-1 tbl-title">{$chert[0]["survey_select"]}</div>
-								<div class="tbl-elem tbl-2 tbl-title">{$chert[1]["survey_select"]}</div>
+								<div class="tbl-elem tbl-1 tbl-title">{$chert[0][0]["survey_select"]}</div>
+								<div class="tbl-elem tbl-2 tbl-title">{$chert[0][1]["survey_select"]}</div>
 							</div>
 				EOD;
 				
-				$all_element_count = round((count($chert[0]["survey_name"]) + count($chert[1]["survey_name"]))/2);
-				for ($i=0; $i <= $all_element_count; $i++) {
-					$chert_elem_0 = isset($chert[0]["survey_name"][$i]) ? $chert[0]["survey_name"][$i] : "";
-					$chert_elem_1 = isset($chert[1]["survey_name"][$i]) ? $chert[1]["survey_name"][$i] : "";
+				$all_element_count = (count($chert[0][0]["survey_name"]) >= count($chert[0][1]["survey_name"])) ? count($chert[0][0]["survey_name"]) : count($chert[0][1]["survey_name"]);
+
+				for ($i=0; $i < $all_element_count; $i++) {
+					$chert_elem_0 = isset($chert[0][0]["survey_name"][$i]) ? $chert[0][0]["survey_name"][$i] : "";
+					$chert_elem_1 = isset($chert[0][1]["survey_name"][$i]) ? $chert[0][1]["survey_name"][$i] : "";
 					echo <<<EOD
 							<div class="table-content">
 								<div class="tbl-elem tbl-1">{$chert_elem_0}</div>
 								<div class="tbl-elem tbl-2">{$chert_elem_1}</div>
 							</div>
 					EOD;
-					// $insert_count++;
 				}
 				echo <<<EOD
-
 						</div>
 					</div>
 				EOD;
@@ -254,13 +305,13 @@ if (isset($survey_result)){
 			new Chart(ctx, {
 			type: 'bar',
 			data: {
-				labels: ["<?php echo $chert[0]["survey_select"];?>", "<?php echo $chert[1]["survey_select"];?>"],
+				labels: ["<?php echo $chert[0][0]["survey_select"];?>", "<?php echo $chert[0][1]["survey_select"];?>"],
 				
 				datasets: [
 					{
 						backgroundColor:["#f2ffe5", "#fff2e5"],
-						label: "<?php echo $chert["title"];?>",
-						data: [<?php echo $chert[0]["count"] ;?>, <?php echo $chert[1]["count"] ;?>],
+						label: "<?php echo $chert[0]["title"];?>",
+						data: [<?php echo $chert[0][0]["count"] ;?>, <?php echo $chert[0][1]["count"] ;?>],
 						borderWidth: 1,
 					}
 				]
